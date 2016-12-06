c165f4d4bc56260ffc63c0dff0c53dd8d661c325
Maor <maorfeldinger@gmail.com>
2016-12-06 11:48:57 +0200

    init app.js controller and service

---
+++ movies_controller.js
@@ -0,0 +1,8 @@
+
+/************movies main controller******************/
+/******Handle all the inputs and outputs*************/
+/****************************************************/
+
+app.controller('moviesController', ['$scope','moviesService', function($scope, moviesService){
+
+}
---
+++ movies_service.js
@@ -0,0 +1,8 @@
+
+/*******************movies main service*********************/
+/*handle main client logic and comunicates with the server**/
+/***********************************************************/
+
+app.factory('moviesService', ['$http', function ($http) {
+
+};
