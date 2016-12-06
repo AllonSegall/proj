5e9c3d5488dfd45420363856767d893dba217bba
Maor <maorfeldinger@gmail.com>
2016-12-06 18:37:35 +0200

    connect random and remove func to html

--- movies_controller.js
+++ movies_controller.js
@@ -5,4 +5,24 @@
 
 app.controller('moviesController', ['$scope','moviesService', function($scope, moviesService){
 
-}
+  var moviesOptions = [];
+
+  $scope.suggestedMovies = moviesOptions;
+
+
+  $scope.btnRand = function(){
+
+      moviesService.getMoviesFromDummy();
+      for(var i = 0; i < 2; i++){
+        moviesOptions[i] = moviesService.getRandMovie();
+      }
+  };
+
+  $scope.btnRemove = function(movie){
+    moviesService.removeMovieFromPull(movie);
+    moviesOptions[moviesOptions.indexOf(movie)] = moviesService.getRandMovie();
+  };
+
+
+
+}]);
